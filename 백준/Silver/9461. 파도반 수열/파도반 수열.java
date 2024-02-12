0027013282ec10e8aaacb3c8931f4eacb971294e
Ho Yeon CHAE <102277048+leonroars@users.noreply.github.com>
2024-02-12 09:56:48 +0900

    [Silver III] Title: 파도반 수열, Time: 120 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -3,6 +3,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+// 풀이 및 접근
+// 동적 계획법은 작은 문제로 분할될 수 있고 실제 답이 Base-case의 결과로부터 차근차근 계산되어 도출될 수 있다는 점에서
+// 재귀와 갈래를 같이 한다고 볼 수 있다. 
+// 하지만 동적 계획법은 기억(Memoization)을 통해 재귀함수적 풀이를 효율적으로 처리할 수 있다.
+
 public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1 << 16); // 버퍼 제한걸어 받기.
@@ -20,6 +25,8 @@ public class Main{
 
     }
 
+    // Nth가 100일 경우 비트 오버플로우가 발생해 음수가 출력된다.
+    // 이렇게 숫자가 int의 4바이트를 초과하게되는 상황도 고려하여 long으로 데이터 타입을 지정해준다.
     static long wave(int Nth){
         long[] memoization = new long[Nth + 1];
         if(Nth < 4){return 1;}
