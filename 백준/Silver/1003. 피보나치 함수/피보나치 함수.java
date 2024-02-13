c21a42a0dec919f4bcaacaa8b100bb277d17d8be
Ho Yeon CHAE <102277048+leonroars@users.noreply.github.com>
2024-02-14 01:00:37 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -2,6 +2,15 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+// 풀이 접근
+// 규칙을 찾는 것이 가장 유효했다.
+// 1보다 큰 n번 째 피보나치 수열의 fib(0) & fib(1) 호출 수는, 각각 n-2, n-1 번째 피보나치 수와 일치한다.
+// 제한 시간이 0.25초로 주어지는 만큼, 일반적인 재귀 함수 작성이 아닌 배열을 이용한 Memoization을 활용하는
+// 동적 계획법적 접근으로 풀이한다.
+// 또한 피보나치 객체를 따로 만들어두고 처음에 인스턴스화시 최대 입력값인 40을 인자로 주어
+// 매 입력마다 피보나치 수열을 불필요하게 반복적으로 재계산할 필요가 없도록 하였다.
+
+
 public class Main{
     public static void main(String[] args) throws IOException {
 
