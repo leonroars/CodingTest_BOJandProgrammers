363b68537ad801d4c3e33f3b801622be1da66e4b
Ho Yeon CHAE <102277048+leonroars@users.noreply.github.com>
2025-05-12 22:13:31 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 120 ms, Memory: 16128 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -5,16 +5,6 @@ import java.util.StringTokenizer;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 
-/*
- 0. 풀이 방식 : 0-1 BFS (Shortest Path Algorithm for Binary Edge-weight Graph)
-             - 간선의 가중치가 0 혹은 1이기 때문에,
-               다음에 방문할 위치를 담은 FIFO Queue에 가중치가 0인 움직임(2 * X), 그 다음에 가중치가 1인 움직임을 추가했다.
-               간선의 가중치가 binary 인 경우 PQ 를 활용한 Dijkstra를 활용하지 않아도 BFS 로도 최단거리 탐색이 가능.
-               단, 후에 더 나은 경로를 찾은 경우를 위해, 만약 어떤 위치의 tab[] 값을 업데이트한 경우 큐에 다시 추가해야한다.
-               그래야만 해당 위치로부터 이어지는 다른 위치로의 도달 최단 시간도 갱신이 가능하기 때문이다.
- 1. 시간 복잡도 : O(K = 100,001) 이다. 기본적으로 해당 알고리즘은 출발지점으로부터 도달가능한 지점을 모두 탐색하기 때문이다.
- */
-
 
 public class Main{
     static int N; // 수빈 위치.
